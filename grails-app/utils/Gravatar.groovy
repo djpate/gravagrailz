f78642fa8818d51248aa194f2828d43b3398ca3b
Christophe Verbinnen <djpate@gmail.com>
2011-06-16 17:01:58 +0200

    Revert "fix: default config"
    This reverts commit 4f72ed81863f1e38e128adc85f9afbd241269345.

--- grails-app/utils/Gravatar.groovy
+++ grails-app/utils/Gravatar.groovy
@@ -10,15 +10,11 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder
 class Gravatar {
 
     static url = { String email, configName='default' ->
-        def config = []
-        config.putAll(defaultConfig)
-        config.putAll(ConfigurationHolder.config.gravagrails.configs[configName])
+        def config = ConfigurationHolder.config.gravagrails.configs[configName]
         String emailHash = email.toLowerCase().encodeAsMD5()
         String url = "http://www.Gravatar.com/avatar/$emailHash?s=${config['size']}&d=${config.imageset}&r=${config.rating}"
         return url
 
     }
-
-    static defaultConfig = [ size: 80, imageset: 'mm', rating: 'g' ]
 
 }
